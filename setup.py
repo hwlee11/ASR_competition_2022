4478be95283646370e899ba611cdff893aa1135f
hwlee <hwl@ai.korea.ac.kr>
2022-08-18 04:06:20 +0900

    init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#nsml: pytorch/pytorch:1.6.0-cuda10.1-cudnn7-runtime
+from distutils.core import setup
+
+setup(
+    name='test',
+    version=0.1,
+    install_requires=[
+        'librosa >= 0.7.0',
+        'numpy',
+        'pandas',
+        'tqdm',
+        'matplotlib',
+        'astropy',
+        'sentencepiece',
+        'torchaudio==0.6.0',
+        'pydub',
+        'glob2'
+    ]
+)
